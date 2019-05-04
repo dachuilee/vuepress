7ed00962870f04174d00ed812600594257a5be21
dachuilee <dachuilee@163.com>
2019-05-04 15:15:03 +0800

    edit press and refresh

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  base:"/vuepress/",
-  title: 'da_chui',
-  description: 'Just up up',
+  base: "/vuepress/",
+  title: "da chui",
+  description: "Just up up",
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: 'https://vuepress.vuejs.org/zh/guide/' },
-      { test:'github',link:"https://github.com/dachuilee/vuepress"}
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "https://vuepress.vuejs.org/zh/guide/" },
+      { text: "github", link: "https://github.com/dachuilee/vuepress" }
     ]
   }
-}
+};
