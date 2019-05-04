2917015606519175029e593e984d4301c1102694
dachuilee <dachuilee@163.com>
2019-05-04 20:46:25 +0800

    edit press and refresh

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,13 +3,36 @@ module.exports = {
   title: "da chui",
   description: "Just up up",
   head: [
-    ['link', { rel: 'icon', href: 'https://github.githubassets.com/favicon.ico' }]
+    [
+      "link",
+      { rel: "icon", href: "https://github.githubassets.com/favicon.ico" }
+    ]
   ],
   themeConfig: {
     nav: [
       { text: "Home", link: "/" },
       { text: "Guide", link: "https://vuepress.vuejs.org/zh/guide/" },
       { text: "github", link: "https://github.com/dachuilee/vuepress" }
-    ]
+    ],
+    sidebar: {
+      "/foo/": [
+        "" /* /foo/ */,
+        "one" /* /foo/one.html */,
+        "two" /* /foo/two.html */
+      ],
+
+      "/bar/": [
+        "" /* /bar/ */,
+        "three" /* /bar/three.html */,
+        "four" /* /bar/four.html */
+      ],
+
+      // fallback
+      "/": [
+        "" /* / */,
+        "contact" /* /contact.html */,
+        "about" /* /about.html */
+      ]
+    }
   }
 };
