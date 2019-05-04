5e8d66c737bcad71896674fd3deb2c7051313de5
dachuilee <dachuilee@163.com>
2019-05-04 13:55:46 +0800

    edit press

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 set -e
 git config user.name dachuilee
 git config user.email dachuilee@163.com
+yarn docs:build
 git add -A
 git commit -m "edit press"
-yarn docs:build
 git push git@github_dachui:dachuilee/vuepress.git master:master
 
 
