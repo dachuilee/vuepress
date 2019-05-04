a915fb91adf4ca6aeb4ebfea1bc17384f570c59f
dachuilee <dachuilee@163.com>
2019-05-04 15:13:29 +0800

    edit press and refresh

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,12 @@
 module.exports = {
   base:"/vuepress/",
   title: 'da_chui',
-  description: 'Just up up'
+  description: 'Just up up',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: 'https://vuepress.vuejs.org/zh/guide/' },
+      { test:'github',link:"https://github.com/dachuilee/vuepress"}
+    ]
+  }
 }
