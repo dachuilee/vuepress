fdf5ffac7628ea707df4876609c8d3f241346369
dachuilee <dachuilee@163.com>
2019-05-12 20:50:35 +0800

    更新配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  base: '/vuepress/',
-  title: 'Hello dachui',
-  description: 'Just playing around'
+  base: '/vuepress/', // pages 根目录
+  port: "9999", // 配置端口
+  title: 'Hello dachui', // vuepress 头部名称
+  description: 'Just playing around', // vuepress 描述
+  permalink: "/:year/:month/:day/:slug",// 永久链接
+  head: [
+    [
+      "link", // 自定义的 favicon
+      { rel: "icon", href: "https://github.githubassets.com/favicon.ico" }
+    ]
+  ],
 }
